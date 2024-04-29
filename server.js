e27ba8742e94b9f88093d0fa879282df3bcabf14
saurabhshashi <saurabhshashi.54@gmail.com>
2024-04-29 19:54:23 +0530

    Updates Added

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get(("/", (req, res) => {
+app.get("/", (req, res) => {
   res.json({ message: "Server is running" });
-}))
+})
 
 app.listen(4000, () => console.log(`Server running on port 4000`));
 
@@ -105,5 +105,5 @@ connect('mongodb+srv://saurabhshashi54:Shashi2580@cluster0.wmocqch.mongodb.net/r
         console.log(`Node API app is running on port 3000`)
     });
 }).catch((error) => {
-    console.log(error)
+    console.log("Database Connection Error: ", error)
 })
