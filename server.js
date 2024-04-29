6774dea0e8977eba15925c983a91b38efed34fa9
saurabhshashi <saurabhshashi.54@gmail.com>
2024-04-29 19:42:04 +0530

    Backend Testing

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get(("/", (req, res) => {
+  res.json({ message: "Server is running" });
+}))
+
 app.listen(4000, () => console.log(`Server running on port 4000`));
 
 // Add new data
